efd6b528c97a43e9b874634cbc2d71d6d64858b2
Markus Mengel <markus.mengelhx@gmail.com>
2020-03-02 18:15:00 +0100

    added relationships in category, user, user_categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+  has_many :user_categories
+  belongs_to :request
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,5 @@
 class Request < ApplicationRecord
   belongs_to :creator, class_name: "User"
   belongs_to :helper, class_name: "User"
+  has_one :category
 end
