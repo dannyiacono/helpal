63bf378ff8e64f59b8dcee395c64c8c16585c55e
Danny Iacono <danny.iacono@contentful.com>
2020-03-02 17:47:22 +0100

    Created requests db

---
+++ db/migrate/20200302163917_create_requests.rb
@@ -0,0 +1,15 @@
+class CreateRequests < ActiveRecord::Migration[5.2]
+  def change
+    create_table :requests do |t|
+      t.text :description
+      t.date :due_date
+      t.integer :status
+      t.string :title
+      t.references :creator, foreign_key: { to_table: :users }
+      t.references :helper, foreign_key: { to_table: :users }
+      t.references :category, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_02_161714) do
+ActiveRecord::Schema.define(version: 2020_03_02_163917) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "requests", force: :cascade do |t|
+    t.text "description"
+    t.date "due_date"
+    t.integer "status"
+    t.string "title"
+    t.bigint "creator_id"
+    t.bigint "helper_id"
+    t.bigint "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_requests_on_category_id"
+    t.index ["creator_id"], name: "index_requests_on_creator_id"
+    t.index ["helper_id"], name: "index_requests_on_helper_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +44,17 @@ ActiveRecord::Schema.define(version: 2020_03_02_161714) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "location"
+    t.integer "age"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "requests", "categories"
+  add_foreign_key "requests", "users", column: "creator_id"
+  add_foreign_key "requests", "users", column: "helper_id"
 end
