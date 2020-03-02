a23cf01a17286215f6ea2ea8b56d68087a21218c
Danny Iacono <danny.iacono@contentful.com>
2020-03-02 17:49:12 +0100

    Created reviews table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_02_163917) do
+ActiveRecord::Schema.define(version: 2020_03_02_164832) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,15 @@ ActiveRecord::Schema.define(version: 2020_03_02_163917) do
     t.index ["helper_id"], name: "index_requests_on_helper_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "content"
+    t.bigint "request_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["request_id"], name: "index_reviews_on_request_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -57,4 +66,5 @@ ActiveRecord::Schema.define(version: 2020_03_02_163917) do
   add_foreign_key "requests", "categories"
   add_foreign_key "requests", "users", column: "creator_id"
   add_foreign_key "requests", "users", column: "helper_id"
+  add_foreign_key "reviews", "requests"
 end
