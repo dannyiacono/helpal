fa7332776d7e64dab2f4e03cde42e7da9fda13ec
Danny Iacono <danny.iacono@contentful.com>
2020-03-02 17:50:47 +0100

    Added user_categories table to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_02_164832) do
+ActiveRecord::Schema.define(version: 2020_03_02_165000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,15 @@ ActiveRecord::Schema.define(version: 2020_03_02_164832) do
     t.index ["request_id"], name: "index_reviews_on_request_id"
   end
 
+  create_table "user_categories", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_user_categories_on_category_id"
+    t.index ["user_id"], name: "index_user_categories_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -67,4 +76,6 @@ ActiveRecord::Schema.define(version: 2020_03_02_164832) do
   add_foreign_key "requests", "users", column: "creator_id"
   add_foreign_key "requests", "users", column: "helper_id"
   add_foreign_key "reviews", "requests"
+  add_foreign_key "user_categories", "categories"
+  add_foreign_key "user_categories", "users"
 end
