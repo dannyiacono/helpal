d4d94105d76b64949136f7d4dcb1a9a467e9396b
Danny Iacono <danny.iacono@contentful.com>
2020-03-03 15:21:16 +0100

    Created user profile, edit, and other user profile views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  patch 'profiles/update'
+  get 'profiles/edit'
+  get '/my_profile/', to: "profiles#my_profile", as: :my_profile
+  get '/profiles/:id', to: "profiles#other_profile", as: :other_profile
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
