c6a264dfad44da823b4d230fb3c6e8657d45752d
Markus Mengel <markus.mengelhx@gmail.com>
2020-03-03 13:52:49 +0100

    aded create method to messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,20 @@
 class MessagesController < ApplicationController
-  def index
-  end
-
-  def show
+  before_action do
+   @conversation = Conversation.find(params[:conversation_id])
   end
 
   def create
+    @message = @conversation.messages.new(message_params)
+    @message.user_id = current_user.id
+    authorize @message
+    if @message.save
+      redirect_to conversation_messages_path(@conversation)
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body, :user_id)
   end
 end
