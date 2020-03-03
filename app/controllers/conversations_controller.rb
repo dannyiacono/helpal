4080e0c3da562aaa4b128ffa3deede353a724589
Markus Mengel <markus.mengelhx@gmail.com>
2020-03-03 15:10:53 +0100

    added interpolation and missing end in converations controller

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -3,25 +3,23 @@ class ConversationsController < ApplicationController
 
   def index
     @conversations = []
-    Conversation.where(creator_id: current_user.id || helper_id: current_user.id).find_each do |conversation|
+    Conversation.where(creator_id: current_user.id).or(Conversation.where(helper_id: current_user.id)).find_each do |conversation|
       @conversations << conversation
     end
   end
 
   def create
- if Conversation.between(params[:creator_id],params[:helper_id])
-   .present?
-    @conversation = Conversation.between(params[:creator_id],
-     params[:helper_id]).first
- else
-  @conversation = Conversation.create!(conversation_params)
- end
- redirect_to conversation_messages_path(@conversation)
-
+    if Conversation.between(params[:creator_id],params[:helper_id]).present?
+      @conversation = Conversation.between(params[:creator_id], params[:helper_id]).first
+    else
+      @conversation = Conversation.create!(conversation_params)
+    end
+    redirect_to conversation_messages_path(@conversation)
+  end
 
   private
 
   def conversation_params
     params.permit(:creator_id, :helper_id)
- end
+  end
 end
