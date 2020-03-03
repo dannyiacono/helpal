affe28385a6574ecd699dc8794463c2ae66c7793
Danny Iacono <danny.iacono@contentful.com>
2020-03-03 15:41:30 +0100

    Added a couple of validations for name and location, added column for bio

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,9 @@ class User < ApplicationRecord
   has_many :messages
   has_many :requests_as_creator, source: :requests, foreign_key: :creator_id
   has_many :requests_as_helper, source: :requests, foreign_key: :helper_id
-  has_many :requests
+  has_many :requests, dependent: :destroy
   has_many :reviews, through: :requests
   has_many :conversations_as_creator, source: :conversations, foreign_key: :creator_id
   has_many :conversations_as_helper, source: :conversations, foreign_key: :helper_id
+  validates :first_name, :last_name, :location, presence: true
 end
