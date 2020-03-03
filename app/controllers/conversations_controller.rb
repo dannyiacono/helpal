cc63bc35977d0896b3c7ac1fa5e900b1f7b72ffe
Markus Mengel <markus.mengelhx@gmail.com>
2020-03-03 15:03:58 +0100

    added index and crate method to conversations controller and basic index view

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,9 +1,20 @@
 class ConversationsController < ApplicationController
   def index
+    @conversations = []
+    Conversation.where(creator_id: current_user.id || helper_id: current_user.id).find_each do |conversation|
+      @conversations << conversation
+    end
   end
 
   def create
-  end
+ if Conversation.between(params[:creator_id],params[:helper_id])
+   .present?
+    @conversation = Conversation.between(params[:creator_id],
+     params[:helper_id]).first
+ else
+  @conversation = Conversation.create!(conversation_params)
+ end
+ redirect_to conversation_messages_path(@conversation)
 
 
   private
