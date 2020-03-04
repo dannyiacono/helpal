98a21abf9119cd6cda6c607dc5162aabc26ca23c
Markus Mengel <markus.mengelhx@gmail.com>
2020-03-04 12:33:31 +0100

    deleted unneeded files

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,13 +3,4 @@ class Message < ApplicationRecord
   belongs_to :conversation
   validates :body, presence: true, allow_blank: true
 
-  def broadcast_message
-    ActionCable.server.broadcast("conversation_#{conversation.id}", {
-      message_partial: ApplicationController.renderer.render(
-        partial: "messages/message",
-        locals: { message: self, user_is_messages_author: false }
-      ),
-      current_user_id: user.id
-    })
-  end
 end
