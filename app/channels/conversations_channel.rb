a546db2a03df1de11f40ede8b943026490823035
Markus Mengel <markus.mengelhx@gmail.com>
2020-03-04 11:17:18 +0100

    save current state

--- app/channels/conversations_channel.rb
+++ app/channels/conversations_channel.rb
@@ -5,5 +5,6 @@ class ConversationsChannel < ApplicationCable::Channel
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+    stream_from "conversation_#{params[:chat_room_id]}"
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,14 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :conversation
   validates :body, presence: true, allow_blank: true
+
+  def broadcast_message
+    ActionCable.server.broadcast("conversation_#{conversation.id}", {
+      message_partial: ApplicationController.renderer.render(
+        partial: "messages/message",
+        locals: { message: self, user_is_messages_author: false }
+      ),
+      current_user_id: user.id
+    })
+  end
 end
