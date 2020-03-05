24835cfed75234e8b0d7ac677a307bd06f5f82a7
Jana Uvarova <uvarova.jana@gmail.com>
2020-03-05 14:04:14 +0100

    added enum to request

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,6 @@
 class Request < ApplicationRecord
+  enum status: { pending: 0, ongoing: 1, done: 2 }
+
   belongs_to :creator, class_name: "User"
   belongs_to :helper, class_name: "User", optional: true
   has_one :category
