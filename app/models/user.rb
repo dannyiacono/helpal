ad18b5fbd30bb9816264090cdf93ef1dcc6f9ede
Danny Iacono <danny.iacono@contentful.com>
2020-03-05 11:45:12 +0100

    Finished the avatar in navbar and profiles.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_one_attached :photo
   has_many :user_categories
   has_many :categories, through: :user_categories
   has_many :messages
