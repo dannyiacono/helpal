b849e0b005e70249252a69e82ef023b204fa8dd6
Markus Mengel <markus.mengelhx@gmail.com>
2020-03-05 11:55:34 +0100

    added relationship between request and conversation to models

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -2,6 +2,7 @@ class Conversation < ApplicationRecord
   belongs_to :sender, foreign_key:  :sender_id, class_name: "User"
   belongs_to :recipient, foreign_key: :recipient_id, class_name: "User"
   has_many :messages
+  belongs_to :request
 
   validates_uniqueness_of :sender_id, scope: :recipient_id
 
