93251292ee35a8494a4b790f1a7d30da436c90c5
Markus Mengel <markus.mengelhx@gmail.com>
2020-03-05 12:44:00 +0100

    replaced sender and recipient by creator and helper

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,11 +1,11 @@
 class Conversation < ApplicationRecord
-  belongs_to :sender, foreign_key:  :sender_id, class_name: "User"
-  belongs_to :recipient, foreign_key: :recipient_id, class_name: "User"
+  belongs_to :creator, foreign_key:  :creator_id, class_name: "User"
+  belongs_to :helper, foreign_key: :helper_id, class_name: "User"
   has_many :messages
   belongs_to :request
 
   validates :request_id, presence: true
-  validates_uniqueness_of :sender_id, scope: :recipient_id
+  validates_uniqueness_of :creator_id, scope: :helper_id
 
   scope :between, -> (creator_id,helper_id) do
  where("(conversations.creator_id = ? AND conversations.helper_id = ? ) OR (conversations.creator_id = ? AND conversations.helper_id = ? )", creator_id,helper_id, helper_id, creator_id)
