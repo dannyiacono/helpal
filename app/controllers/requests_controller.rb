d24779fd0fff0b257389e2c669207c8da6a6046a
Markus Mengel <markus.mengelhx@gmail.com>
2020-03-07 17:50:51 +0100

    fixed typo for success message

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -35,7 +35,7 @@ class RequestsController < ApplicationController
     @request = Request.find(params[:id])
     authorize @request
     @request.destroy
-    redirect_to categories_path, notice: "Succesfully cancelled your request"
+    redirect_to categories_path, notice: "Successfully cancelled your request"
   end
 
   def update
