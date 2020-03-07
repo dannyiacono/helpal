596abf40169df07ddfeec916ddb5f30c66793b73
Markus Mengel <markus.mengelhx@gmail.com>
2020-03-08 00:58:22 +0100

    coloring of status works now for all conversations

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,5 +3,5 @@ import { initStarRating } from '../plugins/init_star_rating';
 import { status_color } from '../plugins/status_color_change';
 
 initStarRating();
-document.querySelector('.conversation-status').onchange= status_color();
+document.querySelectorAll('.conversation-status').onchange= status_color();
 
--- app/javascript/plugins/status_color_change.js
+++ app/javascript/plugins/status_color_change.js
@@ -1,15 +1,17 @@
 const status_color = () => {
-  const conversation_status = document.querySelector('.conversation-status');
+  const conversations_status = document.querySelectorAll('.conversation-status');
 
-  if (conversation_status.textContent == 'pending') {
-    conversation_status.style.color = "black";
-  }
-  else if (conversation_status.textContent == 'ongoing') {
-    conversation_status.style.color = "red";
-  }
-  else {
-    conversation_status.style.color = "green";
-  }
+  conversations_status.forEach(conversation_status => {
+    if (conversation_status.textContent == 'pending') {
+      conversation_status.style.color = "black";
+    }
+    else if (conversation_status.textContent == 'ongoing') {
+      conversation_status.style.color = "red";
+    }
+    else {
+      conversation_status.style.color = "green";
+    }
+  });
 }
 
 export { status_color };
