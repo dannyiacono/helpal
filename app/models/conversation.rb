6846db8b9206fe6e15c90951cce6e62a0af99500
Markus Mengel <markus.mengelhx@gmail.com>
2020-03-07 16:38:27 +0100

    added enum to conversation model

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,4 +1,5 @@
 class Conversation < ApplicationRecord
+  enum status: { pending: 0, declined: 1, confirmed: 2 }
   belongs_to :creator, foreign_key:  :creator_id, class_name: "User"
   belongs_to :helper, foreign_key: :helper_id, class_name: "User"
   has_many :messages
