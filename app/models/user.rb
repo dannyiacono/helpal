cb79efaf22566f6838f36ddcc0693b925050dee0
Danny Iacono <danny.iacono@contentful.com>
2020-03-08 17:24:19 +0100

    Added empty state logic. Added some methods to the user model for accessing reviews.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,30 @@ class User < ApplicationRecord
   has_many :user_categories
   has_many :categories, through: :user_categories
   has_many :messages
-  has_many :requests, dependent: :destroy
+  # has_many :requests, dependent: :destroy
   has_many :requests_as_creator, source: :requests, foreign_key: :creator_id, class_name: "Request"
   has_many :requests_as_helper, source: :requests, foreign_key: :helper_id, class_name: "Request"
   has_many :reviews, through: :requests
   has_many :conversations_as_creator, source: :conversations, foreign_key: :creator_id
   has_many :conversations_as_helper, source: :conversations, foreign_key: :helper_id
   validates :first_name, :last_name, :location, presence: true
+
+  def requests
+    requests_as_creator + requests_as_helper
+  end
+
+  def reviews
+    reviews = []
+    requests.each { |request| reviews << request.review unless request.review.nil?}
+    reviews
+  end
+
+  def reviews_as_request_creator
+    reviews.select { |review| review if review.request.creator == self}
+  end
+
+  def reviews_as_request_helper
+    reviews.select { |review| review if review.request.creator != self}
+  end
+
 end
