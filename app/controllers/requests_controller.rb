4fe0ba831f8b4da9465606919b3397c67203ec27
Mariken Gulpen <marikenvangulpen@gmail.com>
2020-03-10 11:37:08 +0100

    link to conversations

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -15,6 +15,7 @@ class RequestsController < ApplicationController
     @id = @request.helper_id
     @id ? @helper = User.find(@id) : @helper = nil
     @helper_conversation = Conversation.find_by(request_id: @request.id, helper_id: @request.helper_id)
+    @conversation = Conversation.create
   end
 
   def new
