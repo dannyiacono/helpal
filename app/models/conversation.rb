02fade006c511a6a157950b5b1537f767926cd41
Mariken Gulpen <marikenvangulpen@gmail.com>
2020-03-10 13:42:24 +0100

    deleted validation on conversation uniqueness of helper and creator so that I can have anoter conversation between same helper and creator

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -6,9 +6,5 @@ class Conversation < ApplicationRecord
   belongs_to :request
 
   validates :request_id, presence: true
-  validates_uniqueness_of :creator_id, scope: :helper_id
 
-  scope :between, -> (creator_id,helper_id) do
- where("(conversations.creator_id = ? AND conversations.helper_id = ? ) OR (conversations.creator_id = ? AND conversations.helper_id = ? )", creator_id,helper_id, helper_id, creator_id)
- end
 end
