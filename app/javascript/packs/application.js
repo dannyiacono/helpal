6725c46f410f2d998a9d60c4e5568c6db57deeca
Markus Mengel <markus.mengelhx@gmail.com>
2020-03-10 13:59:29 +0100

    changed order of js scripts now coloring works again

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,8 +4,8 @@ import { initStarRating } from '../plugins/init_star_rating';
 import { status_color } from '../plugins/status_color_change';
 import { initAutocomplete } from '../plugins/init_autocomplete';
 
-initAutocomplete();
 
 initStarRating();
 document.querySelectorAll('.conversation-status').onchange= status_color();
 
+initAutocomplete();
