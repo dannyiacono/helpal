3f78daaf4f706a41dfd648e8a0a7bdabfccde4f7
Mariken Gulpen <marikenvangulpen@gmail.com>
2020-03-11 14:35:07 +0100

    notification for no category selected now works

--- app/javascript/packs/categories_index.js
+++ app/javascript/packs/categories_index.js
@@ -3,19 +3,25 @@ const catInput = document.getElementById("request_category_id");
 const catId = document.querySelector(".name_cat_id");
 
 const categoriesIndex = () => {
+
   const categories = document.querySelectorAll(".category-card");
   categories.forEach((card) => {
     card.addEventListener("click", (event) => {
       categories.forEach((category) => {
         category.classList.remove("change-border-color")
-      })
+            })
     event.currentTarget.classList.add("change-border-color");
     catSelect.classList.add('blue-back');
     catSelect.innerText = event.currentTarget.innerText;
     catInput.value = catId.innerText;
+    const catEmpty = document.querySelector(".no-category-selected");
+     catEmpty.classList.add("d-none")
     });
-
   });
+  if(catSelect.innerText === "") {
+    catSelect.insertAdjacentHTML("afterend","<span class='no-category-selected'>Please select a category before continuing.</span>");
+  }
+
 };
 
 
