2d3f78af28db5df04a66eff215fc3e7220403c22
Mariken Gulpen <marikenvangulpen@gmail.com>
2020-03-11 15:06:31 +0100

    fixing crash location

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,13 +2,18 @@ class CategoriesController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
 
   def index
-    @location = params[:query].split(",").first
+    @location = params[:query]
+    if @location.nil?
+      redirect_to root_path
+    else
+      @loco = @location.split(",").first
+    end
     @categories = policy_scope(Category)
     @request = Request.new
     if params[:commit] && params[:commit] != "Search"
       @user_category_selection = params[:commit]
       @category = Category.find_by(name: @user_category_selection)
-      @request = Request.new(city: params[:query])
+      @request = Request.new(city: @loco)
     end
 
   end
