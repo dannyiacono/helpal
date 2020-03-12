d1f9fb8e18bb248af8e72f33cffd04a3fd887047
Markus Mengel <markus.mengelhx@gmail.com>
2020-03-12 11:10:53 +0100

    added scroll script to application js

--- app/javascript/plugins/scrollToLastMessage.js
+++ app/javascript/plugins/scrollToLastMessage.js
@@ -1,8 +1,11 @@
-function scrollLastMessageIntoView() {
-  const messages = document.querySelectorAll('.message');
+const scrollToLastMessage = () => {
+  const messages = document.querySelectorAll('.conversation-messages');
   const lastMessage = messages[messages.length - 1];
 
   if (lastMessage !== undefined) {
     lastMessage.scrollIntoView();
   }
 }
+
+
+export { scrollToLastMessage };
